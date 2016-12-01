3d69bb86b12f8a92ad040bbf8b6a24a862234e6e
wayerr <radiofun@ya.ru>
2016-12-01 14:10:41 +0300

    fix

--- app_api.js
+++ app_api.js
@@ -9,18 +9,22 @@ function () {
         }
         request.headers["X-Auth-Token"] = api.token;
     }
+    function url(suffix) {
+        return api.host + "/ui/api/" + suffix;
+    }
     api.token = null;
     api.login = function(name, password) {
          var request = {
-            url: api.host + "/api/token/login",
+            url: api.host + "/ui/token/login",
             data: { username:name, password:password }
         };
         var resp = http.execute(request);
+        console.debug(" * Login with token: ", JSON.stringify(resp));
         return api.token = resp.data.key;
     };
     api.list = function() {
         var request = {
-            url: api.host + "/api/list"
+            url: url("users-current")
         };
         reqInterceptor(request);
         var resp = http.execute(request);
--- main.js
+++ main.js
@@ -2,6 +2,5 @@ var api = include("app_api");
 function test() {
    api.host = "http://localhost:8761/";
    var token = api.login("admin", "password");
-   console.debug("token:", token);
    console.debug("list:", api.list(token));
 }
