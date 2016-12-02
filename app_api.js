40492f8b1dd45568c68e4994520829374a8cc782
wayerr <radiofun@ya.ru>
2016-12-03 01:44:38 +0300

    add cluster and nodes list

--- app_api.js
+++ app_api.js
@@ -12,6 +12,22 @@ function () {
     function url(suffix) {
         return api.host + "/ui/api/" + suffix;
     }
+    function makeGet(urlSrc) {
+        return function () {
+            var suffix;
+            if(typeof urlSrc === 'function') {
+                suffix = urlSrc.apply(this, arguments);
+            } else {
+                suffix = urlSrc;
+            }
+            var request = {
+                url: url(suffix)
+            };
+            reqInterceptor(request);
+            var resp = http.execute(request);
+            return resp.data;
+        };
+    }
     api.token = null;
     api.login = function(name, password) {
          var request = {
@@ -19,16 +35,10 @@ function () {
             data: { username:name, password:password }
         };
         var resp = http.execute(request);
-        console.debug(" * Login with token: ", JSON.stringify(resp));
+        console.debug(" * Login with token: ", resp.data.key);
         return api.token = resp.data.key;
     };
-    api.list = function() {
-        var request = {
-            url: url("users-current")
-        };
-        reqInterceptor(request);
-        var resp = http.execute(request);
-        return resp.data;
-    };
+    api.clustersList = makeGet("clusters/");
+    api.clusterNodesDetailed = makeGet(function (cluster) {return "clusters/" + cluster + "/nodes-detailed";});
     return api;
 }
--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var api = include("app_api");
 function test() {
-   api.host = "http://localhost:8761/";
-   var token = api.login("admin", "password");
-   console.debug("list:", api.list(token));
+    api.host = process.env.API_HOST || "http://localhost:8761/";
+    console.debug("api.host:", api.host);
+    var token = api.login("admin", "password");
+    var clusters = api.clustersList();
+    var clusterNames = clusters.map(function(c) {return c.name;});
+    console.debug("clusters:", clusterNames);
+    for(var i = 0; i  < clusterNames.length; ++i) {
+        var clusterName = clusterNames[i];
+        var clusterNodes = api.clusterNodesDetailed(clusterName);
+        console.debug("nodes for ", clusterName, ": ", clusterNodes);
+    }
 }
